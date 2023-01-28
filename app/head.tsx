39589835b305420847969be7484f80bf44cab31f
Lon Beshiri <lonbeshiri@gmail.com>
2023-01-28 17:27:29 -0500

    build: initial commit

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Tone [by Sone]</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Tone [by Sone]" />
+      {/*<link rel="icon" href="/favicon.ico" />*/}
+    </>
+  )
+}
