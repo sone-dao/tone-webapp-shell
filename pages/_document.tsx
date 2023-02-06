68801c8c87332f119d5de78898229cdd18dd569a
Lon Beshiri <lonbeshiri@gmail.com>
2023-02-06 16:07:17 -0500

    feat: document and globals

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,16 @@ class ModdedDocument extends Document {
       <Html>
         <Head>
           <link rel="stylesheet" href="https://use.typekit.net/epg1kit.css" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=optional"
