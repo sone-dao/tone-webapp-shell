5597af039cfe0c2cff85b397623070c219ab48b2
Lon Beshiri <lonbeshiri@gmail.com>
2023-02-07 14:00:52 -0500

    feat: removed helmet hide

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,15 +1,8 @@
 import { Registration } from '@/components/admissions/Registration'
-import { pub } from '@sone-dao/sone-react-utils'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useEffect } from 'react'
 
 const RegistrationPage: NextPage = () => {
-  useEffect(() => {
-    pub('__TONE_EVENTS__', 'helmet.hide', true)
-    return () => pub('__TONE_EVENTS__', 'helmet.hide', false)
-  }, [])
-
   return (
     <>
       <Head>
