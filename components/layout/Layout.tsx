0189ea32b40ae5f5253574628606b12ecd52a2c1
Lon Beshiri <lonbeshiri@gmail.com>
2023-02-07 23:57:39 -0500

    refactor: layout clean up & css vars

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,37 +1,18 @@
-import { createContext, useContext, useState } from 'react'
 import { Helmet } from '../core_ui/Helmet'
 import { Sidebar } from '../core_ui/Sidebar'
 import styles from './Layout.module.scss'
-
-interface ILayoutContext {
-  setHelmetHidden: Function
-}
-
-const F = () => {}
-
-const layoutContextDefaults: ILayoutContext = {
-  setHelmetHidden: F,
-}
-
-const LayoutContext = createContext<ILayoutContext>(layoutContextDefaults)
-
-export const useLayoutContext = () => useContext(LayoutContext)
 
 export interface ILayoutProps {
   children: React.ReactNode
 }
 
 const Layout: React.FC<ILayoutProps> = ({ children }) => {
-  const [isHelmetHidden, setHelmetHidden] = useState<boolean>(false)
-
   return (
-    <LayoutContext.Provider value={{ setHelmetHidden }}>
-      <div className={styles.layout}>
-        <Sidebar />
-        {!isHelmetHidden && <Helmet />}
-        <main>{children}</main>
-      </div>
-    </LayoutContext.Provider>
+    <div className={styles.layout}>
+      <Sidebar />
+      <Helmet />
+      <main>{children}</main>
+    </div>
   )
 }
 
