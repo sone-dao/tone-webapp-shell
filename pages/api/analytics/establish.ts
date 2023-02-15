c6410c0a6496013b8197e05cb7f0f18019203170
Lon Beshiri <lonbeshiri@gmail.com>
2023-02-15 11:19:53 -0500

    fix: analytics route

---
+++ pages/api/analytics/establish.ts
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-anonymous-default-export */
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  /* const token = req.headers['authorization']?.split(' ')[1]
+
+  if (!token)
+    return res.status(400).json({ ok: false, message: 'MISSING_TOKEN' })*/
+
+  const sessionData = {
+    ...req.body,
+    ip: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
+  }
+
+  switch (req.method) {
+    case 'POST':
+      return fetch('https://analytics.sone.works/establish', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(sessionData),
+      })
+        .then((response) => response.json())
+        .then((data) => data)
+        .catch((error) => ({ ok: false, error }))
+  }
+}
